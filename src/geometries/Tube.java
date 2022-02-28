0f6ad94376383670f5dc7f4074b99c708b743f34
Avi Yalow <avrahamyalow@gmail.com>
2022-02-28 21:00:04 +0200

    primitivesv

--- src/geometries/Tube.java
+++ src/geometries/Tube.java
@@ -1,6 +1,8 @@
 package geometries;
 
+import primitives.Point;
 import primitives.Ray;
+import primitives.Vector;
 
 public class Tube implements Geometry {
 
@@ -8,5 +10,29 @@ public class Tube implements Geometry {
 
     protected double radius;
 
+    public Tube(Ray axisRay, double radius) {
+        this.axisRay = axisRay;
+        this.radius = radius;
+    }
 
+    public Ray getAxisRay() {
+        return axisRay;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Tube{" +
+                "axisRay=" + axisRay +
+                ", radius=" + radius +
+                '}';
+    }
+
+    @Override
+    public Vector getNormal(Point point) {
+        return null;
+    }
 }
