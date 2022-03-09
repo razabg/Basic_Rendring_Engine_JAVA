ed6bd910bfefaf210bb8c2ddd23ce659ebb52d38
Avi Yalow <avrahamyalow@gmail.com>
2022-03-09 22:30:42 +0200

    vector tests

--- unitTests/primitives/VectorTest.java
+++ unitTests/primitives/VectorTest.java
@@ -23,6 +23,9 @@ class VectorTest {
     }
 
 
+    /**
+     * test method for {@link Vector#lengthSquared()}
+     */
     @Test
     void lengthSquared() {
 
@@ -30,6 +33,9 @@ class VectorTest {
 
     }
 
+    /**
+     * test method for {@link Vector#length()}
+     */
     @Test
     void length() {
         assertTrue(isZero(new Vector(0, 3, 4).length() - 5),"\"ERROR: length() wrong value\"");
@@ -47,10 +53,51 @@ class VectorTest {
 
     @Test
     void crossProduct() {
+
+        // ============ Equivalence Partitions Tests ==============
+        Vector vr = v1.crossProduct(v3);
+
+        // Test that length of cross-product is proper (orthogonal vectors taken for simplicity)
+        assertEquals(v1.length() * v3.length(), vr.length(), 0.00001, "crossProduct() wrong result length");
+
+        // Test cross-product result orthogonality to its operands
+        assertTrue(isZero(vr.dotProduct(v1)), "crossProduct() result is not orthogonal to 1st operand");
+        assertTrue(isZero(vr.dotProduct(v3)), "crossProduct() result is not orthogonal to 2nd operand");
+
+        // =============== Boundary Values Tests ==================
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> v1.crossProduct(v2),
+                "crossProduct() for parallel vectors does not throw an exception"
+        );
+
     }
 
     @Test
     void normalize() {
+        // ============ Equivalence Partitions Tests ==============
+
+
+
+        // =============== Boundary Values Tests ==================
+
+        // test vector normalization vs vector length and cross-product
+        Vector u=v1.normalize();
+        assertTrue(isZero(u.length()-1),"ERROR: the normalized vector is not a unit vector");
+
+        // test that the vectors are co-lined
+        assertThrows(IllegalArgumentException.class,
+                () -> v1.crossProduct(u),
+                "ERROR: the normalized vector is not parallel to the original one");
+
+        /**
+         *
+         * if (v.dotProduct(u) < 0)
+         *             out.println("ERROR: the normalized vector is opposite to the original one");
+         */
+
+
+
 
     }
 }
