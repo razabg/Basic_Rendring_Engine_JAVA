8dd72a26d640929e36452d2a5ad74f5f1990bbe6
razabg <razabg@gmail.com>
2022-05-01 14:38:56 +0300

    ex5 finish

--- src/primitives/Color.java
+++ src/primitives/Color.java
@@ -5,7 +5,7 @@ package primitives;
  * non-negative RGB values. The colors are maintained without upper limit of
  * 255. Some additional operations are added that are useful for manipulating
  * light's colors
- * 
+ *
  * @author Dan Zilberstein
  */
 public class Color {
@@ -19,7 +19,6 @@ public class Color {
 	 * Black color = (0,0,0)
 	 */
 	public static final Color BLACK = new Color();
-	public static final Color BLUE = new Color(0d,0d,255d);
 
 	/**
 	 * Default constructor - to generate Black Color (privately)
@@ -47,7 +46,7 @@ public class Color {
 	 * Constructor to generate a color according to RGB components Each component in
 	 * range 0..255 (for printed white color) or more [for lights]
 	 *
-	 * @param rgb triad of Red/Green/Blue components 
+	 * @param rgb triad of Red/Green/Blue components
 	 */
 	private Color(Double3 rgb) {
 		if (rgb.d1 < 0 || rgb.d2 < 0 || rgb.d3 < 0)
@@ -57,7 +56,7 @@ public class Color {
 
 	/**
 	 * Constructor on base of java.awt.Color object
-	 * 
+	 *
 	 * @param other java.awt.Color's source object
 	 */
 	public Color(java.awt.Color other) {
@@ -96,9 +95,9 @@ public class Color {
 	}
 
 	/**
-	 * Scale the color by a scalar
+	 * Scale the color by a scalar triad per rgb
 	 *
-	 * @param k scale factor
+	 * @param k scale factor per rgb
 	 * @return new Color object which is the result of the operation
 	 */
 	public Color scale(Double3 k) {
@@ -108,8 +107,32 @@ public class Color {
 	}
 
 	/**
+	 * Scale the color by a scalar
+	 *
+	 * @param k scale factor
+	 * @return new Color object which is the result of the operation
+	 */
+	public Color scale(double k) {
+		if (k < 0.0)
+			throw new IllegalArgumentException("Can't scale a color by a negative number");
+		return new Color(rgb.scale(k));
+	}
+
+	/**
 	 * Scale the color by (1 / reduction factor)
-	 * 
+	 *
+	 * @param k reduction factor
+	 * @return new Color object which is the result of the operation
+	 */
+	public Color reduce(double k) {
+		if (k < 1)
+			throw new IllegalArgumentException("Can't scale a color by a by a number lower than 1");
+		return new Color(rgb.reduce(k));
+	}
+
+	/**
+	 * Scale the color by (1 / reduction factor)
+	 *
 	 * @param k reduction factor
 	 * @return new Color object which is the result of the operation
 	 */
